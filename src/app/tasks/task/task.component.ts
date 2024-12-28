cbdb98e65dd41467e3711b03cdc008aad6e48b42
Ziad Abuzayyad <ziad_24389@hotmail.com>
2024-12-28 21:15:05 +0300

    task tried

---
+++ src/app/tasks/task/task.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  imports: [],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.scss'
+})
+export class TaskComponent {
+
+}
