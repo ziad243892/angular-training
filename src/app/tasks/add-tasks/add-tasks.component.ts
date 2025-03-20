8473b778d6dc5c62b7a9347560621bbba53b3316
Ziad Abuzayyad <ziad.abuzayyad@mobisoft-me.com>
2025-03-20 15:01:33 +0300

    learn add task

--- src/app/tasks/add-tasks/add-tasks.component.ts
+++ src/app/tasks/add-tasks/add-tasks.component.ts
@@ -1,16 +1,36 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-tasks',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-tasks.component.html',
   styleUrl: './add-tasks.component.scss'
 })
 export class AddTasksComponent {
+
+  taskTitle:string = '';
+  summaryText:string = ''
+  date:string = '';
+
   @Output() close = new EventEmitter<void>();
-  
+  @Output() add = new EventEmitter<{
+    title: string,
+    summary: string,
+    date: string
+  }>();
+
+
   onClose() {
     this.close.emit();
+  }
+
+  onSubmit() {
+    this.add.emit({
+        title: this.taskTitle,
+        summary: this.summaryText,
+        date: this.date,
+    });
   }
 }
